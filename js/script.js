c39694c56cc14c2e7976aca8254d7d861d70216f
Adilson-Moraes <moraes.adilson94@gmail.com>
2023-01-17 12:09:06 -0300

    finalizado filtro com js

--- js/script.js
+++ js/script.js
@@ -89,9 +89,35 @@ $(document).ready(function () {
     }
   });
 
-  //Parallax
+  //Portofolio
+  $(".filter-btn").on("click", function () {
+    let type = $(this).attr("id");
+    let boxes = $(".project-box");
 
-  setTimeout(function () {
-    $("#data-area").parallax({ imageSrc: "img/cidadeparallax.png" });
-  }, 250);
+    $(".main-btn").removeClass("active");
+    $(this).addClass("active");
+
+    if (type == "dsg-btn") {
+      eachBoxes("dsg", boxes);
+    } else if (type == "dev-btn") {
+      eachBoxes("dev", boxes);
+    } else if (type == "seo-btn") {
+      eachBoxes("seo", boxes);
+    } else {
+      eachBoxes("all", boxes);
+    }
+  });
+  function eachBoxes(type, boxes) {
+    if (type == "all") {
+      $(boxes).fadeIn();
+    } else {
+      $(boxes).each(function () {
+        if (!$(this).hasClass(type)) {
+          $(this).fadeOut("slow");
+        } else {
+          $(this).fadeIn();
+        }
+      });
+    }
+  }
 });
