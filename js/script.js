da5569075bee309f6f10b7dfb35fa842e9025af9
Adilson-Moraes <moraes.adilson94@gmail.com>
2023-01-18 09:28:41 -0300

    finalização do html/css/js do projeto

--- js/script.js
+++ js/script.js
@@ -88,36 +88,36 @@ $(document).ready(function () {
       stop = 1;
     }
   });
+});
+//Portofolio
 
-  //Portofolio
-  $(".filter-btn").on("click", function () {
-    let type = $(this).attr("id");
-    let boxes = $(".project-box");
+$(".filter-btn").on("click", function () {
+  let type = $(this).attr("id");
+  let boxes = $(".project-box");
 
-    $(".main-btn").removeClass("active");
-    $(this).addClass("active");
+  $(".main-btn").removeClass("active");
+  $(this).addClass("active");
 
-    if (type == "dsg-btn") {
-      eachBoxes("dsg", boxes);
-    } else if (type == "dev-btn") {
-      eachBoxes("dev", boxes);
-    } else if (type == "seo-btn") {
-      eachBoxes("seo", boxes);
-    } else {
-      eachBoxes("all", boxes);
-    }
-  });
-  function eachBoxes(type, boxes) {
-    if (type == "all") {
-      $(boxes).fadeIn();
-    } else {
-      $(boxes).each(function () {
-        if (!$(this).hasClass(type)) {
-          $(this).fadeOut("slow");
-        } else {
-          $(this).fadeIn();
-        }
-      });
-    }
+  if (type == "dsgbtn") {
+    eachBoxes("dsg", boxes);
+  } else if (type == "devbtn") {
+    eachBoxes("dev", boxes);
+  } else if (type == "seobtn") {
+    eachBoxes("seo", boxes);
+  } else {
+    eachBoxes("all", boxes);
   }
 });
+function eachBoxes(type, boxes) {
+  if (type == "all") {
+    $(boxes).fadeIn();
+  } else {
+    $(boxes).each(function () {
+      if (!$(this).hasClass(type)) {
+        $(this).fadeOut("slow");
+      } else {
+        $(this).fadeIn();
+      }
+    });
+  }
+}
